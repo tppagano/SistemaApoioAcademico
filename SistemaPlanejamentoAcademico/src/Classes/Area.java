8a827def4cf03896c9dfe7cf58a0b7db13544dc6
tiago <tiago@tiago-HP-Pavilion-14-Notebook-PC>
2017-08-17 17:02:39 -0300

    Classe área

--- SistemaPlanejamentoAcademico/src/Classes/Area.java
+++ SistemaPlanejamentoAcademico/src/Classes/Area.java
@@ -1,12 +1,26 @@
 package Classes;
 
+import java.util.Date;
+
 public class Area {
 
-
-	public Area()
+	private String nome; 
+	
+	public Area(String nome)
 	{
+	
+		setNome(nome);
 		
 		
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
 	
